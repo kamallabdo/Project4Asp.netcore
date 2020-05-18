00390564c46c4dc0152f83eb0eabbb1eb805c109
kamallabdo <Abderahmane.kamal@gmail.com>
2020-05-18 04:19:11 +0000

    first commit

---
+++ Exo3_1/Controllers/LoginController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Exo3_1.models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Exo3_1.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors("MyPolicy")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        // GET: api/Login
+        private readonly IConfiguration _config;
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        private readonly IEnumerable<User> _users = new List<User>
+        {
+            new User {id=1,Username="kamal",Password="123",Role="adminstrator"},
+            new User {id=2,Username="Abdo",Password="123",Role="user"}
+
+        };
+
+        private String GenerateJSONWebToken(User userInfo)
+        {
+            var user = _users.Where(x => x.Username == userInfo.Username && x.Password == userInfo.Password).SingleOrDefault();
+            if (user == null)
+            {
+                return null;
+            }
+            var signingKey = Convert.FromBase64String(_config["Jwt:key"]);
+            var expiryDuration = int.Parse(_config["Jwt:ExpiryDuration"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Issuer = null,
+                Audience = null,
+                IssuedAt = DateTime.UtcNow,
+                NotBefore = DateTime.UtcNow,
+                Expires = DateTime.UtcNow.AddMinutes(expiryDuration),
+                Subject = new ClaimsIdentity(new List<Claim>
+                {
+                    new Claim("userid",user.id.ToString()),
+                    new Claim("roles",user.Role),
+                    new Claim("Username",user.Username.ToString())
+                }),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(signingKey), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+            var jwtToken = jwtTokenHandler.CreateJwtSecurityToken(tokenDescriptor);
+            var token = jwtTokenHandler.WriteToken(jwtToken);
+            return token;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] User user)
+        {
+            var jwtToken = GenerateJSONWebToken(user);
+            if (jwtToken == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(jwtToken);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult Get()
+        {
+            var currentUser = HttpContext.User.Claims.Where(x => x.Type == "userid").SingleOrDefault();
+            return Ok($"utilisateur connecte a un id"+ currentUser.Value);
+        }
+        [HttpGet]
+        [Route("gestion")]
+        [Authorize(Roles = "adminstrator")]
+        public IActionResult Gestion()
+        {
+            var currentUser = HttpContext.User.Claims.Where(x => x.Type == "userid").SingleOrDefault();
+            return Ok($"utilisateur connecte a un id" + currentUser.Value);
+           
+        }
+        
+    }
+}
---
+++ Exo3_1/Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Exo3_1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+              .AddJwtBearer(option =>
+              {
+                  var signinKey = Convert.FromBase64String(Configuration["Jwt:key"]);
+                  option.TokenValidationParameters = new TokenValidationParameters
+                  {
+                      ValidateIssuer = false,
+                      ValidateAudience = false,
+                      ValidateLifetime = true,
+                      ValidateIssuerSigningKey = true,
+                      ValidIssuer = Configuration["Jwt:Issuer"],
+                      ValidAudience = Configuration["Jwt:Issuer"],
+                      IssuerSigningKey = new SymmetricSecurityKey(signinKey)
+                  };
+              });
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseCors("MyPolicy");
+
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
